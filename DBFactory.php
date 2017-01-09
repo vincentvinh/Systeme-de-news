43ab86aa6f6f9400323ac9a0444b9c832a04c41d
vincent <vincent_christophe@live.fr>
2017-01-09 16:39:51 +0100

    Ajouté tous les fichiers vierge de changement

---
+++ DBFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+class DBFactory
+
+{
+
+  public static function getMysqlConnexionWithPDO()
+
+  {
+
+    $db = new PDO('mysql:host=localhost;dbname=news', 'root', '');
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+
+    return $db;
+
+  }
+
+  
+
+  public static function getMysqlConnexionWithMySQLi()
+
+  {
+
+    return new MySQLi('localhost', 'root', '', 'news');
+
+  }
+
+}
+?>
---
+++ autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+function autoload($classname)
+
+{
+
+  if (file_exists($file = __DIR__ . '/' . $classname . '.php'))
+
+  {
+
+    require $file;
+
+  }
+
+}
+
+
+spl_autoload_register('autoload');
+?>
